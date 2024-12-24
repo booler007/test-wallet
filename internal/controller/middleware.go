c6f53496ddb520c1443734e5d13b4302c392cb56
Ilya Postnikov <sewerefd@gmail.com>
2024-12-24 11:55:14 +0700

    feat: add middleware for handling errors

--- internal/controller/middleware.go
+++ internal/controller/middleware.go
@@ -1,8 +1,11 @@
 package controller
 
 import (
+	"errors"
 	"log"
 	"net/http"
+
+	"wallet/internal/service"
 
 	"github.com/gin-gonic/gin"
 )
@@ -15,6 +18,10 @@ func ErrorHandler() gin.HandlerFunc {
 			switch {
 			case err.Type == gin.ErrorTypeBind:
 				ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": gin.H{err.Error(): err.Meta}})
+			case errors.Is(err, service.ErrInsufficientBalance):
+				ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			case errors.Is(err, service.ErrWalletNotFound):
+				ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			default:
 				log.Print()
 				ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
