6bec3e9c3748f29a01119ea61531552c8f9d0dc9
Ilya Postnikov <sewerefd@gmail.com>
2024-12-24 11:54:54 +0700

    feat: change structure of DB

--- migrations/0001_create_tables.down.sql
+++ migrations/0001_create_tables.down.sql
@@ -1,2 +1,1 @@
-DROP TABLE IF EXISTS transactions;
 DROP TABLE IF EXISTS wallets;
--- migrations/0001_create_tables.up.sql
+++ migrations/0001_create_tables.up.sql
@@ -1,13 +1,4 @@
 CREATE TABLE IF NOT EXISTS wallets(
     uuid UUID PRIMARY KEY,
-    balance numeric(10, 2) NOT NULL
-);
-
-CREATE TYPE operation_type AS ENUM ('DEPOSIT', 'WITHDRAW');
-CREATE TABLE IF NOT EXISTS transactions (
-    uuid UUID PRIMARY KEY,
-    transaction_time timestamp,
-    wallet UUID references wallets (uuid),
-    operation operation_type,
-    amount numeric(10, 2)
+    balance int NOT NULL
 );
